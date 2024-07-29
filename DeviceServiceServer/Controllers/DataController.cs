511010fb25b244de639e5ef511d0d28619d5ae67
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-07-29 22:54:20 +0200

    ADD controller DataController

---
+++ DeviceServiceServer/Controllers/DataController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeviceServiceServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult StoreSensoreData([FromBody] object request)
+        {
+            if (request == null)
+            {
+                return BadRequest("Request body is null");
+            }
+
+            return Ok("Request received and processed successfully.");
+        }
+    }
+}
